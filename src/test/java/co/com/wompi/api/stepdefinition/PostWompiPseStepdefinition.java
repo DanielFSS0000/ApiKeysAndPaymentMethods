ddfb97a3a27fa02c84a928b97f54af3a944d0f28
Daniel Sandoval <daniel970620@gmail.com>
2025-08-08 14:52:32 -0500

    Modify task and Stepddefinition pse

--- src/test/java/co/com/wompi/api/stepdefinition/PostWompiPseStepdefinition.java
+++ src/test/java/co/com/wompi/api/stepdefinition/PostWompiPseStepdefinition.java
@@ -1,4 +1,84 @@
 package co.com.wompi.api.stepdefinition;
 
+import co.com.wompi.api.models.PseTransactionRequest;
+import co.com.wompi.api.tasks.post.CreatePseTransaction;
+import co.com.wompi.api.utils.Constants;
+import co.com.wompi.api.utils.ReferenceGenerator;
+import co.com.wompi.api.utils.SignatureUtils;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.core.environment.EnvironmentSpecificConfiguration;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.util.EnvironmentVariables;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostWompiPseStepdefinition {
+
+    private EnvironmentVariables environmentVariables;
+
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @When("the user creates a PSE transaction in Wompi with approving bank")
+    public void userCreatesPseTransactionApprovingBank() {
+
+        String wompiBaseUrlKey = Constants.BASE_URL.replace(Constants.TYPE_ENVIRONMENT, "sandbox");
+        String wompiBaseUrl = EnvironmentSpecificConfiguration.from(environmentVariables)
+                .getProperty(wompiBaseUrlKey);
+
+        String acceptanceToken = SerenityRest
+                .given()
+                .get(wompiBaseUrl + "merchants/" + Constants.PUBLIC_KEY_WOMPI)
+                .jsonPath()
+                .getString("data.presigned_acceptance.acceptance_token");
+
+        PseTransactionRequest req = new PseTransactionRequest();
+        req.amount_in_cents = 1000000;
+        req.currency = "COP";
+        req.customer_email = "pruebasensandbox@yopmail.com";
+        req.reference = ReferenceGenerator.randomNequiReference();
+        req.acceptance_token = acceptanceToken;
+
+        req.payment_method = new PseTransactionRequest.PaymentMethod();
+        req.payment_method.type = "PSE";
+        req.payment_method.user_type = 0;               // 0: Natural
+        req.payment_method.user_legal_id_type = "CC";   // CC o NIT
+        req.payment_method.user_legal_id = "1999888777";
+        req.payment_method.financial_institution_code = "1";  // 1 = APROBADA (sandbox)
+        req.payment_method.payment_description = "Pago Tienda Wompi"; // <= 30 chars
+
+
+        req.signature = SignatureUtils.calcularFirmaIntegridad(
+                req.reference, req.amount_in_cents, req.currency, Constants.INTEGRITY_KEY_WOMPI
+        );
+
+        OnStage.theActorCalled(Constants.ACTOR).attemptsTo(
+                CreatePseTransaction.with(req, wompiBaseUrl, Constants.PRIVATE_KEY_WOMPI)
+        );
+    }
+
+    @Then("the PSE response status should be {string}")
+    public void theResponseStatusShouldBePending(String expectedReason) {
+        String actualReason = SerenityRest.lastResponse().jsonPath().getString("data.status");
+        assertThat(actualReason, equalTo(expectedReason));
+    }
+
+    @Then("the payment method should be {string}")
+    public void thePaymentMethodShouldBePSE(String expectedReason) {
+        OnStage.theActorInTheSpotlight().should(
+                seeThat(
+                        actor -> SerenityRest.lastResponse().jsonPath()
+                                .getString("data.payment_method.type"),
+                        equalTo("PSE")
+                )
+        );
+    }
 }
